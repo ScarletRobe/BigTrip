6108506ef7884b8ad640d87ccd039a6b1f4ba00e
Leonid Chernyakov <scrltrb@gmail.com>
2022-08-21 19:10:34 +0300

    Refactor: initialize all constants

--- src/consts.js
+++ src/consts.js
@@ -1,18 +1,29 @@
 const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+
+// Waypoint
+const BASE_PRICE_RANGE = [300, 1500];
+
+// Offers
 const OFFERS = ['Choose meal', 'Choose seats', 'Upgrade to comfort class', 'Upgrade to business class', 'Add luggage', 'Business lounge'];
+const OFFER_PRICE_RANGE = [20, 500];
+
+// Destinations
 const CITIES = ['Chamonix', 'Geneva', 'Oslo', 'Helsinki', 'Amsterdam'];
 const CITIES_DESCRIPTIONS = [', middle-eastern paradise, a perfect place to stay with a family.', ', with an embankment of a mighty river as a centre of attraction.', ', a true asian pearl.', ', with a beautiful old town.'];
 const PICTURES_DESCRIPTIONS = ['street market', 'city centre', 'kindergarten', 'parliament building', 'zoo', 'central station'];
-
-const OFFERS_RANGE = [0, 5];
 const PICTURES_RANGE = [1, 10];
+
+
+const TRIP_EVENTS_AMOUNT = 10;
 
 export {
   TYPES,
   OFFERS,
-  OFFERS_RANGE,
   CITIES,
   CITIES_DESCRIPTIONS,
   PICTURES_DESCRIPTIONS,
   PICTURES_RANGE,
+  TRIP_EVENTS_AMOUNT,
+  OFFER_PRICE_RANGE,
+  BASE_PRICE_RANGE,
 };
