342bc11486836b9d2943f8d262d1a8874405f298
Leonid Chernyakov <scrltrb@gmail.com>
2022-08-25 21:16:08 +0300

    Feat: add template for empty waypoints list

---
+++ src/view/empty-list-view.js
@@ -0,0 +1,40 @@
+import { createElement } from '../render.js';
+
+/**
+ * Возвращает шаблон элемента фильтрации событий.
+ * @returns {string} строка с HTML кодом.
+ */
+const getEmptyListTemplate = () => (
+  `<section class="trip-events">
+    <h2 class="visually-hidden">Trip events</h2>
+
+    <p class="trip-events__msg">Click New Event to create your first point</p>
+
+    <!--
+      Значение отображаемого текста зависит от выбранного фильтра:
+        * Everthing – 'Click New Event to create your first point'
+        * Past — 'There are no past events now';
+        * Future — 'There are no future events now'.
+    -->
+  </section>`
+);
+
+export default class EmptyListView {
+  #element = null;
+
+  get template () {
+    return getEmptyListTemplate();
+  }
+
+  get element () {
+    if(!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
