48ded41ef776425e2fc2625c168eda5a47d9a2ae
Leonid Chernyakov <scrltrb@gmail.com>
2022-10-03 23:56:22 +0300

    Refactor: small changes

--- src/view/error-message-view.js
+++ src/view/error-message-view.js
@@ -1,11 +1,11 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-const loadingTemplate = () => (
+const errorMessageTemplate = () => (
   '<p class="trip-events__msg">Loading error. Please try again later.</p>'
 );
 
 export default class ErrorMessageView extends AbstractView {
   get template() {
-    return loadingTemplate();
+    return errorMessageTemplate();
   }
 }
